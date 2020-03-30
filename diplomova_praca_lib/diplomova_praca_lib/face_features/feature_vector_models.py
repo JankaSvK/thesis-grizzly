9840ff32e05f4838044d95a96e7802081a621c36
Jana Bátoryová <janulik11@gmail.com>
2020-03-30 18:04:00 +0200

    Faces locations obtained in batch

--- diplomova_praca_lib/diplomova_praca_lib/face_features/feature_vector_models.py
+++ diplomova_praca_lib/diplomova_praca_lib/face_features/feature_vector_models.py
@@ -6,11 +6,14 @@ from diplomova_praca_lib.face_features.models import FaceDetection
 from diplomova_praca_lib.models import EvaluationMechanism
 from diplomova_praca_lib.position_similarity.models import Crop
 
+
 class EvaluatingFaces(EvaluationMechanism):
     def features(self, images):
-        return [self.face_features(image) for image in images]
+        face_locations = face_recognition.batch_face_locations(images, number_of_times_to_upsample=3,
+                                                               batch_size=len(images))
+        return [self.face_features(image=image, locations=faces) for faces, image in zip(face_locations, images)]
 
-    def face_features(self, image: PIL.Image, use_cnn = True):
+    def face_features(self, image: PIL.Image, use_cnn=True, locations=None):
         """
         Returns a position of all detected faces on the image with their feature encoding.
         :param image: PIL.Image
@@ -18,10 +21,13 @@ class EvaluatingFaces(EvaluationMechanism):
         """
         np_image = image_processing.pil_image_to_np_array(image)
 
-        if use_cnn:
-            face_locations = face_recognition.face_locations(np_image, model='cnn')
+        if locations == None:
+            if use_cnn:
+                face_locations = face_recognition.face_locations(np_image, model='cnn')
+            else:
+                face_locations = face_recognition.face_locations(np_image)
         else:
-            face_locations = face_recognition.face_locations(np_image)
+            face_locations = locations
 
         if not face_locations:
             return []
@@ -35,3 +41,4 @@ class EvaluatingFaces(EvaluationMechanism):
                 Crop(top=top / im_height, left=left / im_width, bottom=bottom / im_height, right=right / im_width))
 
         return [FaceDetection(location, encoding) for location, encoding in zip(face_locations_relative, face_features)]
+
